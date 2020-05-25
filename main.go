683b05c355e2e3992dd02980beb0c457f2c6ef09
yangyu10 <yangyu@aibee.com>
2020-05-25 11:54:08 +0800

    增加命令行解析参数

--- main.go
+++ main.go
@@ -7,9 +7,16 @@ import (
     "syscall"
     "os"
     "os/signal"
+    "flag"
 )
 
 func main(){
+    var (
+        port             = flag.Int("port", 8088, "server listen port")
+        downloadDir      = flag.String("dir", "download", "download dir")
+    
+    )
+    flag.Parse()
     s := make(chan os.Signal, 1)
     signal.Notify(s, syscall.SIGUSR1)
     go func () {
@@ -19,10 +26,14 @@ func main(){
             fmt.Println("Reloaded config")
         }
     }()
+    err := os.MkdirAll(*downloadDir, 0777)
+    if err != nil {
+        fmt.Printf("create dir failed,err :%s", err)
+    }
     cfg := config.Config()
     fmt.Println(cfg.DB.Server)
     fmt.Println(cfg.Owner.Name)
-    server.Start()
+    server.Start(*port)
     fmt.Println("started...")
 
 }
--- server/server.go
+++ server/server.go
@@ -1,12 +1,14 @@
 package server
 import (
     "github.com/gin-gonic/gin"
+    "fmt"
 )
 
-func Start() {
+func Start(port int) {
     router := gin.Default()
     handler := &Handler{}
     router.GET("/ping",handler.Ping)
     router.GET("/time", handler.DisplayTime)
-    router.Run(":8888")
+    addr := fmt.Sprintf(":%d", port)
+    router.Run(addr)
 }
