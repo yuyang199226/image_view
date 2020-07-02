c8091816340b3d176491963b4ef47bd60f26fab8
yangyu10 <yangyu@aibee.com>
2020-07-02 19:06:39 +0800

    xx

--- utils/utils.go
+++ utils/utils.go
@@ -4,6 +4,18 @@ import (
 	"time"
 )
 
+func PathExists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return false, err
+}
+
+
 func CurDatetime() string {
 	return time.Now().Format("2006-01-02 15:04:05")
 }
